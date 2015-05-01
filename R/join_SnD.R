852dba8adbaaf9360f7f7f686281726328bc9bf2
aritrab <aritrabiswas.2008@gmail.com>
2015-05-01 13:18:04 +0300

    NA to 0

--- R/join_SnD.R
+++ R/join_SnD.R
@@ -11,5 +11,9 @@ join_SnD<-function(df1,df2){
   df<-df%>%
     select(-9,-10,-12:-16,-18:-20)
   names(df)<-c("APIURL","DOP","Catagory","Subcatagory","Title","Details","videoURL","ChannelName","UserID","CommentsCount","FavCount","ViewConunt","DislikeCount","LikesCount","UploaderID","VideoID","AvgRt","NoodRaters","FBShareCount","FBLikeCount","FBCommentsCount","TwitterShareCount")
+  df<-as.data.frame(df);df<-dplyr::tbl_df(df);df[is.na(df)]<-0;
+  df$CommentsCount<-as.numeric(as.character(df$CommentsCount)); df$FavCount<-as.numeric(as.character(df$FavCount)); df$ViewConunt<-as.numeric(as.character(df$ViewConunt)); df$DislikeCount<-as.numeric(as.character(df$DislikeCount)); df$LikesCount<-as.numeric(as.character(df$LikesCount));df$AvgRt<-as.numeric(as.character(df$AvgRt));
+  df$NoodRaters<-as.numeric(as.character(df$NoodRaters));df$FBShareCount<-as.numeric(as.character(df$FBShareCount));df$FBLikeCount<-as.numeric(as.character(df$FBLikeCount));df$FBCommentsCount<-as.numeric(as.character(df$FBCommentsCount));df$TwitterShareCount<-as.numeric(as.character(df$TwitterShareCount))
+  df[is.na(df)]<-0
   return(df)
 }
